c688546e350b3e30c147c849f5308127a3cba875
Natalie-Cardot <nataliecardot@gmail.com>
2021-05-06 16:31:14 -0700

    Add function that displays end time

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('display__end-time');
 
 function timer(seconds) {
   const initialTime = Date.now();
   // Seconds is multiplied by 1000 first (to convert to milliseconds)
   const stopTime = initialTime + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(stopTime);
 
   countdown = setInterval(() => {
     // Taking initial time with 60 seconds added. When timer begins and this runs every second, Date.now() will increase, meaning difference between stopTime and new current time will decrease, and each second, updated difference is displayed until seconds remaining is 0
@@ -26,4 +28,16 @@ function displayTimeLeft(seconds) {
   }${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
+}
+
+// Show end time (if it's 2:30 and you have a five-minute break, it should say be back at 2:35)
+function displayEndTime(endTimestamp) {
+  // Turn it into date
+  const end = new Date(endTimestamp);
+  const hour = end.getHours();
+  adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${adjustedHour}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}`;
 }
